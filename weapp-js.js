7b865045cfed2f02ec522275ab6fa53b921c385a
Víctor <vsmvsmseg@gmail.com>
2018-01-17 17:52:19 +0100

    Update
    real-time weather: WORKING

--- weapp-js.js
+++ weapp-js.js
@@ -1,40 +1,89 @@
+$(document).ready(function(){
 
-var lat, lon, info;
+	var lat, lon, fullUrl;
 
+	function getLocation() {
+		if (navigator.geolocation) {
+			//browser supports geolocation:
+			navigator.geolocation.getCurrentPosition(showPosition, showError);
+		} else {
+			//if ur browser does not support:
+			alert("Geolocation is not supported by this browser.");
+		}
+	}
 
-function getLocation() {
-    if (navigator.geolocation) {
-        //ur browser support geolocation:
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        //if ur browser does not support:
-        alert("Geolocation is not supported by this browser.");
-    }
-}
+	//FIRST => get latitude and longitude in order to create the url with current coords:
+	function showPosition(position) {
+		lat = position.coords.latitude ;
+		lon = position.coords.longitude;
+		fullUrl = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+lon;
+		
+		//Check the url:
+		console.log(fullUrl);
+		
+		//THEN => make the request with ajax:
+		$.ajax({
+			//request from:
+			url: fullUrl,
+			
+			//If ok:
+			success: function(resp){
+				//$("#verif").html(resp);
+				$("#city").html(resp.name);
+				$("#temp").html(Math.floor(resp.main.temp)+"&ordm;C");
+				$("#sky").html(resp.weather[0].main);
+				console.log("OK");
+				
+			}
+		});
+	}
 
-//get the latitude and longitude defined:
-function showPosition(position) {
-    info = position;
-    lat = "Latitude: " + position.coords.latitude ;
-    lon = "Longitude: " + position.coords.longitude;
-}
-
-//Error messages:
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.")
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.")
-            break;
-        case error.TIMEOUT:
-            alert("The request to get user location timed out.")
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("An unknown error occurred.")
-            break;
-    }
-}
-
-getLocation();
+	//error messages:
+	function showError(error) {
+		switch(error.code) {
+			case error.PERMISSION_DENIED:
+				document.getElementById("err-console").innerHTML = "User denied the request for Geolocation.";
+				break;
+			case error.POSITION_UNAVAILABLE:
+				document.getElementById("err-console").innerHTML = "Location information is unavailable.";
+				break;
+			case error.TIMEOUT:
+				document.getElementById("err-console").innerHTML = "The request to get user location timed out.";
+				break;
+			case error.UNKNOWN_ERROR:
+				document.getElementById("err-console").innerHTML = "An unknown error occurred.";
+				break;
+		}
+	}
+	
+	//STATUS: asignar y mostrar los datos >>>>> HECHO >>>>> en tiempo real >>>> HECHO
+	//
+	
+	
+	/*-------------------------EXAMPLE RESPONSE from reqWeatherApi----------------------
+	
+	{coord: {…}, weather: Array(1), base: "stations", main: {…}, visibility: 10000, …}
+		base:"stations"
+		clouds:{all: 0}
+		cod:200
+		coord:{lon: -3.67, lat: 40.46}
+		dt:1516194000
+		id:6324376
+		main:{temp: 13, pressure: 1025, humidity: 58, temp_min: 13, temp_max: 13}
+		name:"Pinar de Chamartin"
+		sys:{type: 1, id: 5488, message: 0.0062, country: "ES", sunrise: 1516174489, …}
+		visibility:10000
+		weather: //EJ: weather[0].main
+			0:{id: 800, main: "Clear", description: "clear sky", icon: "https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F01d.png?1499366022009"}
+			length:1
+			__proto__:Array(0)
+		wind:{speed: 3.6, deg: 320}
+		__proto__:Object
+		
+	---------------------------------------------------------------------------------*/	
+	
+	getLocation();
+	
+	
+	
+});
